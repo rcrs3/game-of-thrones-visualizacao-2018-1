4d775e35a06219c6a9946c5920732d41a2a94867
rcrs3 <rcrs3@cin.ufpe.br>
2018-06-27 18:27:39 -0300

    Organize files

--- map.js
+++ map.js
@@ -1,11 +1,14 @@
+const kingdom = 'https://raw.githubusercontent.com/rcrs3/game-of-thrones-visualizacao-2018-1/master/kingdoms.geojson';
+
 this.map = L.map('mapid').setView([10, 30], 4);
         L.tileLayer(
           'https://cartocdn-ashbu.global.ssl.fastly.net/ramirocartodb/api/v1/map/named/tpl_756aec63_3adb_48b6_9d14_331c6cbc47cf/all/{z}/{x}/{y}.png',
           { crs: L.CRS.EPSG4326 }).addTo(this.map);
-
+        
         function loadGeojson () {
-          let geojsonElem = document.getElementById('geojson-text-area')
-          geojsonText = JSON.parse(geojsonElem.value)
+          console.log(json + ".geojson");
+          
+          geojsonText = JSON.parse(kingdom)
           console.log(geojsonText);
           L.geoJSON(geojsonText, {
             onEachFeature: onEachFeature
